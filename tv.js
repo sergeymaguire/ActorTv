879da72ea67ec3561dafaf1a52ec3a8935dbd805
sergeymaguire <sergeymaguire@gmail.com>
2018-10-19 08:07:15 -0700

    A node application

--- tv.js
+++ tv.js
@@ -1,22 +1,15 @@
 var request = require("request");
 var fs = require("fs");
-
-// Create the TV constructor
 var TV = function() {
- // divider will be used as a spacer between the tv data we print in log.txt
  var divider =
    "\n------------------------------------------------------------\n\n";
 
- // findShow takes in the name of a tv show and searches the tvmaze API
  this.findShow = function(show) {
    var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
 
    request(URL, function(err, response, body) {
-     // parse the response body (string) to a JSON object
+
      var jsonData = JSON.parse(body);
-
-
-     // showData ends up being the string containing the show data we will print to the console
      var showData = [
        "Show: " + jsonData.name,
        "Genre(s): " + jsonData.genres.join(", "),
@@ -26,8 +19,6 @@ var TV = function() {
      ].join("\n\n");
 
      console.log(showData)
-
-     // Append showData and the divider to log.txt, print showData to the console
      fs.appendFile("log.txt", showData + divider, function(err) {
        if (err) throw err;
        console.log(showData);
