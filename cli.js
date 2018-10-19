3f8e45e9862590dd276a232093fc80b55b8df799
sergeymaguire <sergeymaguire@gmail.com>
2018-10-19 08:08:25 -0700

    A node application

--- cli.js
+++ cli.js
@@ -1,19 +1,12 @@
 var TV = require("./tv");
-
-// Create a new TV object
 var tv = new TV();
-
-// Grab search command line argument
 var search = process.argv[2];
-// Joining the remaining arguments since an actor or tv show name may contain spaces
 var term = process.argv.slice(3).join(" ");
 
-// By default, if no search type is provided, search for a tv show
 if (!search) {
   search = "show";
 }
 
-// By default, if no search term is provided, search for "Andy Griffith"
 if (!term) {
   term = "Andy Griffith";
 }
